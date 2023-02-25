a714f193278fcb08e99877158dadb5beed6d937f
sam10012001 <124744435+sam10012001@users.noreply.github.com>
2023-02-24 18:04:22 -0600

    Add files via upload

---
+++ sam/js/script.js
@@ -0,0 +1,19 @@
+function mostrarMensaje() {
+	var mensaje = "El protocolo HTTP fue desarrollado por Tim Berners-Lee en 1991 mientras trabajaba en el CERN (Organización Europea para la Investigación Nuclear).";
+	document.getElementById("mensaje").innerHTML = mensaje;
+}
+
+function ensenarmensaje() {
+	var mensajee = "La versión original de HTTP, conocida como HTTP/0.9, solo permitía la transferencia de documentos HTML simples y no admitía imágenes, scripts o estilos CSS.";
+	document.getElementById("mensajee").innerHTML = mensajee;
+}
+
+function em3() {
+	var mensaje3 = "La versión más utilizada de HTTP en la actualidad es HTTP/1.1, que fue lanzada en 1999 y se utiliza para la mayoría de las solicitudes y respuestas en la web.";
+	document.getElementById("mensaje3").innerHTML = mensaje3;
+}
+
+function em4() {
+	var mensaje4 = "Los códigos de estado HTTP se utilizan para indicar el resultado de una solicitud HTTP. El código 200 indica que la solicitud fue exitosa, mientras que los códigos 400 y 500 indican errores del cliente y del servidor, respectivamente.";
+	document.getElementById("mensaje4").innerHTML = mensaje4;
+}
